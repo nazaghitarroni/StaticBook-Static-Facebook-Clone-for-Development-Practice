c3773770bd6abf8e9ff88ae13f386e4e40fc3be6
Nazareno Ghitarroni <naghitarroni@uade.edu.ar>
2023-12-29 20:35:13 -0300

    firts commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			color:{
+				grisFondo: '#F0F2F5',
+			}
+		},
+	},
+	plugins: [],
+}
